916cccd6b152b296ce9e41bf4c9819b723213a68
hirad990 <iamhirad17@gmail.com>
2026-09-05 14:23:23 +0330

    feat: expose real launcher and local profile IPC

--- src/preload.js
+++ src/preload.js
@@ -3,6 +3,10 @@ const { contextBridge, ipcRenderer } = require('electron')
 contextBridge.exposeInMainWorld('biner', {
   appVersion: () => ipcRenderer.invoke('app:get-version'),
   openExternal: url => ipcRenderer.invoke('app:open-external', url),
+  getProfile: () => ipcRenderer.invoke('profile:get'),
+  saveProfile: profile => ipcRenderer.invoke('profile:save', profile),
+  launchMinecraft: options => ipcRenderer.invoke('minecraft:launch', options),
+  minecraftStatus: () => ipcRenderer.invoke('minecraft:status'),
   window: {
     minimize: () => ipcRenderer.send('window:minimize'),
     maximize: () => ipcRenderer.send('window:maximize'),
